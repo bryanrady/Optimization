303ded8c0cc6f9b3e830a753e3741b3c6b48d073
bryanrady <bryanrady@163.com>
2020-01-09 17:28:43 +0800

    内存泄漏例子

---
+++ app/src/main/java/com/bryanrady/optimization/advertisement/PlayerActivity.java
@@ -0,0 +1,25 @@
+package com.bryanrady.optimization.advertisement;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author: wangqingbin
+ * @date: 2020/1/9 17:11
+ */
+public class PlayerActivity extends AppCompatActivity {
+//https://www.cnblogs.com/ygj0930/p/7742996.html
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        screenSaverResource.add("https://www.baidu.com/img/bd_logo1.png");
+//
+//        screenSaverResource.add("http://vd3.bdstatic.com/mda-jinpn2ghm53htxvb/sc/mda-jinpn2ghm53htxvb.mp4");
+//
+//        screenSaverResource.add("https://oimagec3.ydstatic.com/image?id=-928318424180310316&product=adpublish&w=520&h=347");
+//
+//        screenSaverResource.add("http://vd2.bdstatic.com/mda-jhffxctu0n1tvhhd/sc/mda-jhffxctu0n1tvhhd.mp4");
+    }
+}
