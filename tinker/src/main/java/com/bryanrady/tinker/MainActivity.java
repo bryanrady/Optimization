1705b0ec4e6224fe00790c3b19d98efc6f5982b1
bryanrady <bryanrady@163.com>
2020-03-27 14:04:19 +0800

    Tinker热修复原理工程搭建。

---
+++ tinker/src/main/java/com/bryanrady/tinker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bryanrady.tinker;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author: wangqingbin
+ * @date: 2020/3/27 12:06
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
